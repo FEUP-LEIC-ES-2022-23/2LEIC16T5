12b39a063a67ed77b9088acf24f28521e622aac1
pedrobarbeira <73803346+pedrobarbeira@users.noreply.github.com>
2023-03-14 09:14:58 +0000

    Delete fortuneko directory

--- fortuneko/app/src/main/java/com/example/fortuneko/exception/LanguageNotSupportedException.java
+++
@@ -1,9 +0,0 @@
-package com.example.fortuneko.exception;
-
-public class LanguageNotSupportedException extends Exception{
-
-    public LanguageNotSupportedException(String language){
-        super(String.format("Language [%s] is not supported\n", language));
-    }
-
-}
--- fortuneko/app/src/main/java/com/example/fortuneko/exception/NotificationTypeNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.fortuneko.exception;
-
-public class NotificationTypeNotFoundException extends Exception{
-    public NotificationTypeNotFoundException(String notificationType){
-        super(String.format("Notification type [%s] not found\n", notificationType));
-    }
-}

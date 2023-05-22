a7d7945ef9c8162006c84d027ed53af95386ff37
HaoChang Fu <fuhaochang664961423@hotmail.com>
2023-05-22 17:59:32 +0100

    added tests

---
+++ app/lib/Viewer/Elements/BarGraph.dart
@@ -0,0 +1,177 @@
+import 'package:es/Model/BudgetBarModel.dart';
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+
+class MyBarGraph extends StatefulWidget {
+  const MyBarGraph(
+      {super.key,
+      required this.barsData,
+      required this.barWidth,
+      required this.graphMaxY,
+      required this.spaceBetweenBars,
+      required this.graphContainerHeight});
+  final List<BudgetBarModel> barsData;
+  final double graphContainerHeight;
+  final double barWidth;
+  final double spaceBetweenBars;
+  final double graphMaxY;
+
+  @override
+  State<MyBarGraph> createState() => BarGraphState();
+}
+
+class BarGraphState extends State<MyBarGraph> {
+  bool barTouched = false;
+
+  @override
+  Widget build(BuildContext context) {
+    double totalWidth = widget.barsData.length *
+                (widget.barWidth + widget.spaceBetweenBars) >
+            MediaQuery.of(context).size.width
+        ? widget.barsData.length * (widget.barWidth + widget.spaceBetweenBars)
+        : MediaQuery.of(context).size.width;
+
+    return Row(
+      children: [
+        Expanded(
+          child: Card(
+            elevation: 4,
+            color: Colors.black,
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Container(
+                height: widget.graphContainerHeight,
+                width: totalWidth,
+                child: Padding(
+                  padding: EdgeInsets.all(30),
+                  child: BarChart(
+                    BarChartData(
+                      alignment: BarChartAlignment.center,
+                      maxY: widget.graphMaxY,
+                      groupsSpace: widget.spaceBetweenBars,
+                      barTouchData: BarTouchData(
+                          enabled: true,
+                          touchCallback: (p0, p1) {
+                            if (mounted) {
+                              setState(() {
+                                barTouched = p0.isInterestedForInteractions;
+                              });
+                            }
+                          },
+                          touchTooltipData: BarTouchTooltipData(
+                            tooltipBgColor: Colors.white,
+                            direction: TooltipDirection.bottom,
+                            tooltipMargin: -55,
+                            getTooltipItem: (group, groupIndex, rod, rodIndex) {
+                              return BarTooltipItem(
+                                  'Limit: ',
+                                  TextStyle(
+                                      color: Color(
+                                          widget.barsData[group.x].color!),
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18),
+                                  children: [
+                                    TextSpan(
+                                        text: widget.barsData[group.x].limit
+                                            .toString(),
+                                        style: TextStyle(
+                                            color: Color(widget
+                                                .barsData[group.x].color!),
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.w500)),
+                                    TextSpan(text: '\n'),
+                                    TextSpan(
+                                        text: 'Value: ',
+                                        style: TextStyle(
+                                            color: Color(widget
+                                                .barsData[group.x].color!),
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.bold)),
+                                    TextSpan(
+                                        text: widget.barsData[group.x].y
+                                            .toString(),
+                                        style: TextStyle(
+                                            color: Color(widget
+                                                .barsData[group.x].color!),
+                                            fontSize: 16,
+                                            fontWeight: FontWeight.w500)),
+                                  ]);
+                            },
+                          )),
+                      gridData: FlGridData(show: false),
+                      borderData: FlBorderData(show: false),
+                      titlesData: FlTitlesData(
+                          show: true,
+                          topTitles: AxisTitles(
+                              drawBehindEverything: true,
+                              sideTitles: SideTitles(
+                                  showTitles: false,
+                                  reservedSize: 30,
+                                  getTitlesWidget: getTopTilesStatus)),
+                          leftTitles: AxisTitles(
+                              sideTitles: SideTitles(showTitles: false)),
+                          rightTitles: AxisTitles(
+                              sideTitles: SideTitles(showTitles: false)),
+                          bottomTitles: AxisTitles(
+                              sideTitles: SideTitles(
+                                  showTitles: true,
+                                  getTitlesWidget: getBottomTiles))),
+                      barGroups: widget.barsData
+                          .map((budgetBarData) =>
+                              BarChartGroupData(x: budgetBarData.x!, barRods: [
+                                BarChartRodData(
+                                    toY: budgetBarData.y!,
+                                    width: widget.barWidth,
+                                    color: Color(budgetBarData.color!),
+                                    borderRadius: BorderRadius.circular(4),
+                                    borderSide: budgetBarData.overLimit!
+                                        ? BorderSide(
+                                            color:
+                                                Color.fromARGB(255, 254, 17, 0),
+                                            style: BorderStyle.solid,
+                                            width: 3)
+                                        : null,
+                                    backDrawRodData: BackgroundBarChartRodData(
+                                        color: budgetBarData.onLimit!
+                                            ? Color.fromARGB(177, 176, 12, 12)
+                                            : Color.fromARGB(
+                                                152, 224, 224, 224),
+                                        show: true,
+                                        toY: budgetBarData.limit))
+                              ]))
+                          .toList(),
+                    ),
+                    swapAnimationDuration: Duration(milliseconds: 500),
+                    swapAnimationCurve: Curves.linear,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget getBottomTiles(double xVal, TitleMeta meta) {
+    dynamic style = TextStyle(
+        color: Color(widget.barsData[xVal.toInt()].color!),
+        fontSize: 14,
+        fontWeight: FontWeight.bold);
+    Widget text =
+        Text(widget.barsData[xVal.toInt()].categoryName!, style: style);
+    return SideTitleWidget(axisSide: meta.axisSide, child: text);
+  }
+
+  Widget getTopTilesStatus(double xVal, TitleMeta meta) {
+    return widget.barsData[xVal.toInt()].onLimit!
+        ? const SizedBox(
+            child: Icon(
+              Icons.warning_amber_rounded,
+              size: 30,
+              color: Colors.red,
+            ),
+          )
+        : const Text('');
+  }
+}
---
+++ app/test/Controller/SavingsMenuController_test.dart
@@ -0,0 +1,55 @@
+import 'dart:async';
+
+import 'package:es/Controller/BudgetMenuController.dart';
+import 'package:es/Controller/SavingsMenuController.dart';
+import 'package:es/Model/BudgetBarModel.dart';
+import 'package:es/Model/TransactionsModel.dart';
+import 'package:es/database/RemoteDBHelper.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:quickalert/models/quickalert_type.dart';
+import 'package:quickalert/widgets/quickalert_dialog.dart';
+import 'BudgetMenuController_test.mocks.dart';
+
+class FakeSavingsMenuController extends Fake implements SavingsMenuController {
+
+ 
+  
+  @override
+  Future updateSavingValue(BuildContext context, String? name_, double currVal,
+      double valueToAdd, bool ifAdd) async {
+    if ((!ifAdd && currVal - valueToAdd >= 0) || (ifAdd)) {
+      QuickAlert.show(
+          context: context,
+          type: QuickAlertType.success,
+          text: "Item updated successfully!");
+      await remoteDBHelper.updateSavingValue(name_, currVal, valueToAdd, ifAdd);
+    } else {
+      QuickAlert.show(
+          context: context,
+          type: QuickAlertType.error,
+          text: "Value to update leads to a negative saving!");
+    }
+  }
+}
+
+@GenerateNiceMocks([MockSpec<RemoteDBHelper>(), MockSpec<BuildContext>()])
+void main() {
+  late MockRemoteDBHelper dbMock;
+  late FakeSavingsMenuController concreteSavingsController;
+  late MockBuildContext buildContextMock;
+  setUpAll(() {
+    dbMock = MockRemoteDBHelper();
+    buildContextMock = MockBuildContext();
+    
+  });
+
+  group('updateSavingValue test', () {
+    test('behaviour test', () {
+      
+    });
+  });
+}
